8fec42d9049087fe86d07b8a3f57066ab207c221
erlosshex <gugsabr@gmail.com>
2018-09-09 15:27:56 +0800

    a replaceable unistd.h for windows
    the IDE is VS2015

---
+++ unistd4win.h
@@ -0,0 +1,6 @@
+// we can create a replaceable unistd.h by the below code -- named by unistd4win.h
+#ifndef _UNISTD_H
+	#define _UNISTD_H
+	#include <io.h>
+	#include <process.h>
+#endif /* _UNISTD_H */
