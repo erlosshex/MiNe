4f49ee76b2efa178a8981418d197a16f168a1730
erlosshex <gugsabr@gmail.com>
2018-09-19 14:15:10 +0800

    test for C++ bit shift

---
+++ test18080601.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char *argv)
+{
+	int i = 1;
+	int j = 1 << 3;
+	std::cout << "i : " << i << std::endl;
+	std::cout << "j : " << j << std::endl;
+	//
+	char c;
+	std::cin >> c;
+	return 0;
+}
