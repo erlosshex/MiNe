23b4ed6acb87a47e0758e29a5cc9b78b97a40f20
erlosshex <gugsabr@gmail.com>
2019-08-22 20:32:53 +0800

    move rectangle
    move rectangle

--- ImageUtility.cpp
+++ ImageUtility.cpp
@@ -11,6 +11,15 @@ namespace ImageUtility {
     cv::Mat matRotateClockWise180(cv::Mat _src);
     // rotate mat --- clockwise 270
     cv::Mat matRotateClockWise270(cv::Mat _src);
+    // move rectangle
+    cv::Rect moveRect(const cv::Rect &rect, int x, int y);
+    cv::Rect moveRect(const cv::Rect &rect, const Point &offset);
+    cv::Rect moveRect(cv::Rect *rect_p, int x, int y);
+    cv::Rect moveRect(cv::Rect *rect_p, const Point &offset);
+    bool moveRectATO(cv::Rect &rect, int x, int y);
+    bool moveRectATO(cv::Rect &rect, const Point &offset);
+    bool moveRectATO(cv::Rect *rect_p, int x, int y);
+    bool moveRectATO(cv::Rect *rect_p, const Point &offset);
 }
 
 //**************************************************************//
@@ -44,3 +53,24 @@ cv::Mat ImageUtility::matRotateClockWise270(cv::Mat _src){
     flip(src_copy, src_copy, 0);
     return src_copy;
 }
+
+cv::Rect ImageUtility::moveRect(const cv::Rect &rect, int x, int y){
+    cv::Rect res = rect;
+    
+    try{
+        res.x += x;
+        res.y += y;
+    }
+    catch(std::exception &e){
+        res = rect;
+    }
+    
+    return res;
+}
+
+cv::Rect moveRect(cv::Rect *rect_p, int x, int y);
+cv::Rect moveRect(cv::Rect *rect_p, const Point &offset);
+bool moveRectATO(cv::Rect &rect, int x, int y);
+bool moveRectATO(cv::Rect &rect, const Point &offset);
+bool moveRectATO(cv::Rect *rect_p, int x, int y);
+bool moveRectATO(cv::Rect *rect_p, const Point &offset);
