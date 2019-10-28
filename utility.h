ef98ebcf5522c31af05cf029283fb21e007cdd2a
erlosshex <gugsabr@gmail.com>
2019-10-28 11:47:50 +0800

    add getFileNameVecFromDir
    add getFileNameVecFromDir

--- utility.h
+++ utility.h
@@ -6,3 +6,4 @@
 using namespace std;
 
 void getAllFiles(string path, vector<string>& files);
+bool getFileNameVecFromDir(const std::string &dirPath, std::vector<std::string> &fileNameVec, std::vector<std::string> &dirNameVec);
